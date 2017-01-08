9f221e113449bfa7bd1a42a3541caa34c4394825
jasunde <jlaurits@gmail.com>
2017-01-08 16:57:16 -0600

    Add get categories by form name

--- server/routes/categories.js
+++ server/routes/categories.js
@@ -19,6 +19,37 @@ router.get('/', function (req, res) {
   })
 })
 
+router.get('/:formName', function (req, res) {
+  var formNames = [
+    'food_rescue',
+    'food_drive',
+    'daily_dist',
+    'sub_dist'
+  ];
+
+  var isFormName = formNames.some(function(name) {
+    return name === req.params.formName
+  })
+
+  if(isFormName) {
+    pool.query(
+      'SELECT * FROM categories '+
+      'WHERE ' + req.params.formName + '=true'
+    )
+    .then(function (result) {
+      res.send(result.rows)
+    })
+    .catch(function (err) {
+      console.log('GET by form error:', err);
+      res.status(500).send(err)
+    })
+  } else {
+    console.log('GET by form error:', req.params.formName, 'is not a valid form name.')
+    res.status(400).send('GET by form error: "' + req.params.formName + '" is not a valid form name.')
+  }
+
+})
+
 router.post('/', function (req, res) {
   var category = req.body
   pool.query(
