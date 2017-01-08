b9deb9834a4c723d31532cdafdaaff2d61d016d5
jasunde <jlaurits@gmail.com>
2017-01-08 14:06:09 -0600

    Users get, post, put

--- server/app.js
+++ server/app.js
@@ -1,11 +1,20 @@
 var express = require('express');
 var app = express();
 var path = require('path');
+var bodyParser = require('body-parser');
+
+// source routes
+var users = require('./routes/users')
+
+app.use(bodyParser.json())
+
 app.set('port', process.env.PORT || '3000');
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, '../public/index.html'));
 });
+
+app.use('/users', users)
 
 app.use(express.static('public'));
 
