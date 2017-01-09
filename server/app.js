8dd51c66dce3239aa29d85e576e79d4d668c028a
jasunde <jlaurits@gmail.com>
2017-01-09 15:32:12 -0600

    Rearrange middleware to make it work

--- server/app.js
+++ server/app.js
@@ -9,19 +9,21 @@ var users = require('./routes/users')
 var categories = require('./routes/categories')
 
 
-app.use(bodyParser.json())
-app.use(decoder)
 app.set('port', process.env.PORT || '3000');
+
+app.use(express.static('public'));
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, '../public/views/index.html'));
 });
 
+app.use(bodyParser.json())
+app.use(decoder)
+
 // route the routes
 app.use('/users', users)
 app.use('/categories', categories)
 
-app.use(express.static('public'));
 
 app.listen(app.get('port'), function () {
   console.log('Listening on port', app.get('port'));
