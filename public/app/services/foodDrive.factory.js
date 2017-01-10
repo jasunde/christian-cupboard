a75bde5227c3011481d6f64fc5b9e6b610aeded0
Brian Matney <brianamatney@gmail.com>
2017-01-09 19:17:36 -0600

    form populating from database

---
+++ public/app/services/foodDrive.factory.js
@@ -0,0 +1,58 @@
+app.factory("FoodDriveFactory", ["$http", "Auth", function($http, Auth){
+
+//   var categories = undefined;
+//   var organizations = undefined;
+//   var idToken = undefined;
+
+
+//   idToken = Auth.idToken();
+
+//   //categories get request
+//   function getTableCategories(){
+//     var promise = $http({
+//       method: 'GET',
+//       url: '/categories',
+//       headers: {
+//         id_token: idToken
+//       }
+//     }).then(function(response){
+//       categories = response.data;
+//       return categories;
+//     });
+//     return promise;
+//   }
+
+//   function getTableOrganizations(){
+//     var promise = $http({
+//       method: 'GET',
+//       url: '/organizations',
+//       headers: {
+//         id_token: idToken
+//       }
+//     }).then(function(response){
+//       organizations = response.data;
+//       return organizations;
+//     });
+//     return promise;
+//   }
+
+//   //food drive API
+//   var foodDriveApi = {
+//     categories: function(){
+//       return categories;
+//     },
+//     organizations: function(){
+//       return organizations;
+//     },
+//     getCategories: function(){
+//       return getTableCategories();
+//     },
+//     getOrganizations: function(){
+//       return getTableOrganizations();
+//     },
+
+//   };
+
+//   return foodDriveApi;
+
+}]);
