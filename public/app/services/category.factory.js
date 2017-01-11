e502d3e579fde73638c303398df9f595f5783623
jasunde <jlaurits@gmail.com>
2017-01-11 10:46:05 -0600

    Harmonize controllers with new auth methodology

--- public/app/services/category.factory.js
+++ public/app/services/category.factory.js
@@ -1,27 +1,29 @@
 app.factory("CategoryFactory", ["$http", "Auth", function ($http, Auth) {
-
+  var verbose = false;
   var categories = {
     list: null
   };
 
   function getCategories() {
-    if(Auth.idToken()) {
+    if(Auth.user.idToken) {
+      if(verbose) {console.log('getting categories');}
       $http({
         method: 'GET',
         url: '/categories',
         headers: {
-          id_token: Auth.idToken()
+          id_token: Auth.user.idToken
         }
       })
         .then(function (result) {
+          if(verbose) {console.log('list', categories.list);}
           categories.list = result.data;
-          console.log('list', categories.list);
         })
         .catch(function (err) {
           console.log('GET categories error:', err);
           categories.list = null;
         })
     } else {
+      if(verbose) {console.log('Not token, no categories');}
       categories.list = null;
     }
   }
