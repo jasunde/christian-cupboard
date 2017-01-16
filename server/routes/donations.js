232ca1aba2fb12ae7c5a53866ede303e883d2c4e
jasunde <jlaurits@gmail.com>
2017-01-16 16:27:02 -0600

    Start correcting contact portion of route

--- server/routes/donations.js
+++ server/routes/donations.js
@@ -153,22 +153,25 @@ router.delete('/:id', function (req, res) {
 router.use(contactService.find)
 router.use(function (req, res, next) {
   // Contacts managed by admin
-  if(req.body.org_type === 'sub_dist' || req.body.org_type === 'food_rescue') {
-    next()
+  if(req.contact) {
+    if(req.body.org_type === 'food_rescue') {
+      next()
 
-  // Contacts NOT managed by admin
-  } else {
-    req.body.donor = true
-    if(req.body.org_name) { 
-      req.body.org = true
-      req.body.org_type = 'donor'  
+    // Contacts NOT managed by admin
+    } else {
+      req.body.donor = true
+      if(req.body.org_name) { 
+        req.body.org = true
+        req.body.org_type = 'donor'  
+      }
+
+      contactService.upsert(req, res)
+        .then(function (response) {
+          req.body.contact_id = req.contact.id
+          next()
+        })
     }
-
-    contactService.upsert(req, res)
-      .then(function (response) {
-        req.body.contact_id = req.contact.id
-        next()
-      })
+  } else {
   }
 })
 
