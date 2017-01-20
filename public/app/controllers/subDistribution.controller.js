098c4ddcbe68461359b1f37e680d4601bbdba6e7
Christopher Mattox <metroplex02@gmail.com>
2017-01-19 20:07:07 -0600

    Admin controls for all views

--- public/app/controllers/subDistribution.controller.js
+++ public/app/controllers/subDistribution.controller.js
@@ -104,4 +104,24 @@ app.controller("SubDistributionController", ['$scope', 'Auth', 'CategoryFactory'
     })
   }
 
+  var today = new Date();
+  var dd = today.getDate();
+  var mm = today.getMonth()+1; //January is 0!
+  var yyyy = today.getFullYear();
+  if(dd<10) {
+      dd='0'+dd
+  }
+  if(mm<10) {
+      mm='0'+mm
+  }
+  today = mm+'/'+dd+'/'+yyyy;
+
+
+    var now = new Date().getTime();
+      $scope.date = new Date(2015, 10, 10);
+      $scope.ago = now < $scope.date.getTime();
+      $scope.before = now > $scope.date.getTime();
+      $scope.startDate = new Date(today);
+      $scope.endDate = new Date(today);
+
 }]);
